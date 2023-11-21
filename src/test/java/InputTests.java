dea1483f9681a76d674a6f3768fd2731cba58dd2
rubuy-74 <rubemviscard2635@gmail.com>
2023-11-21 10:20:19 +0000

    Implement tests for UserInput and Menu

---
+++ src/test/java/InputTests.java
@@ -0,0 +1,60 @@
+import com.aor.tombmask.GUI.GUI;
+import com.aor.tombmask.utils.ACTION;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InputTests {
+    GUI gui;
+
+    public InputTests() throws IOException {
+        this.gui = new GUI();
+    }
+
+    @Test
+    public void userInputUPTest() throws IOException {
+        KeyStroke keyStroke = Mockito.mock(KeyStroke.class);
+        Mockito.when(keyStroke.getKeyType()).thenReturn(KeyType.ArrowUp);
+        assertEquals(gui.getAction(keyStroke), ACTION.UP);
+    }
+
+    @Test
+    public void userInputDOWNTest() throws IOException {
+        KeyStroke keyStroke = Mockito.mock(KeyStroke.class);
+        Mockito.when(keyStroke.getKeyType()).thenReturn(KeyType.ArrowDown);
+        assertEquals(gui.getAction(keyStroke), ACTION.DOWN);
+    }
+
+    @Test
+    public void userInputLEFTTest() throws IOException {
+        KeyStroke keyStroke = Mockito.mock(KeyStroke.class);
+        Mockito.when(keyStroke.getKeyType()).thenReturn(KeyType.ArrowLeft);
+        assertEquals(gui.getAction(keyStroke), ACTION.LEFT);
+    }
+
+    @Test
+    public void userInputRIGHTTest() throws IOException {
+        KeyStroke keyStroke = Mockito.mock(KeyStroke.class);
+        Mockito.when(keyStroke.getKeyType()).thenReturn(KeyType.ArrowRight);
+        assertEquals(gui.getAction(keyStroke), ACTION.RIGHT);
+    }
+
+    @Test
+    public void userInputEXITTest() throws IOException {
+        KeyStroke keyStroke = Mockito.mock(KeyStroke.class);
+        Mockito.when(keyStroke.getKeyType()).thenReturn(KeyType.Escape);
+        assertEquals(gui.getAction(keyStroke), ACTION.EXIT);
+    }
+
+    @Test
+    public void userInputSELECTTest() throws IOException {
+        KeyStroke keyStroke = Mockito.mock(KeyStroke.class);
+        Mockito.when(keyStroke.getKeyType()).thenReturn(KeyType.Enter);
+        assertEquals(gui.getAction(keyStroke), ACTION.ENTER);
+    }
+}
---
+++ src/test/java/MenuTests.java
@@ -0,0 +1,37 @@
+import com.aor.tombmask.model.Menu;
+import com.aor.tombmask.utils.MENU_OPTION;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MenuTests {
+    @Test
+    public void defaultSelectedOptionTest() {
+        Menu menu = new Menu();
+        assertEquals(MENU_OPTION.PLAY,menu.getCurrentOption());
+    }
+    @Test
+    public void movingSelectionMenuTest() {
+        Menu menu = new Menu();
+        menu.upMenu();
+        assertEquals(MENU_OPTION.PLAY,menu.getCurrentOption());
+        menu.downMenu();
+        assertEquals(MENU_OPTION.EXIT,menu.getCurrentOption());
+        menu.downMenu();
+        menu.upMenu();
+        assertEquals(MENU_OPTION.PLAY,menu.getCurrentOption());
+        menu.downMenu();
+        menu.downMenu();
+        assertEquals(MENU_OPTION.EXIT,menu.getCurrentOption());
+    }
+    @Test
+    public void isSelectedTest() {
+        Menu menu = new Menu();
+        assertFalse(menu.isSelected(MENU_OPTION.EXIT));
+        assertTrue(menu.isSelected(MENU_OPTION.PLAY));
+
+        menu.downMenu();
+        assertTrue(menu.isSelected(MENU_OPTION.EXIT));
+        assertFalse(menu.isSelected(MENU_OPTION.PLAY));
+    }
+}
