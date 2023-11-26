b561f2f411f5ebadf5718fceaf16b9c0f674a89b
rubuy-74 <rubemviscard2635@gmail.com>
2023-11-26 14:23:52 +0000

    implement alternative readme file

--- src/main/java/com/dra/tombmask/controller/MenuController.java
+++ src/main/java/com/dra/tombmask/controller/MenuController.java
@@ -25,7 +25,8 @@ public class MenuController extends AbstractController<Menu>{
                 getModel().downMenu();
                 break;
             case ENTER:
-                if(getModel().isSelected(MENU_OPTION.PLAY)) { game.setState(new GameState(new Arena(60, 30, String.format("docs/level%s", game.currentArena)))); }
+                if(getModel().isSelected(MENU_OPTION.PLAY)) {
+                    game.setState(new GameState(new Arena(60, 30, String.format("docs/level%s", game.currentArena)))); }
                 else { game.setState(null); }
         }
     }
