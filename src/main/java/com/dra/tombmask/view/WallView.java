656f2047232d7bf4e9e7c00b5e6ef61f199b88f0
rubuy-74 <rubemviscard2635@gmail.com>
2023-11-29 14:47:03 +0000

    Refactor View Classes

--- src/main/java/com/dra/tombmask/view/WallView.java
+++ src/main/java/com/dra/tombmask/view/WallView.java
@@ -1,10 +1,17 @@
 package com.dra.tombmask.view;
 
 import com.dra.tombmask.GUI.GUI;
+import com.dra.tombmask.model.Position;
 import com.dra.tombmask.model.Wall;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
 
 public class WallView implements ElementView<Wall>{
     public void draw(Wall wall, GUI gui){
-        gui.drawWall(wall.getPosition());
+        TextGraphics textGraphics = gui.getScreen().newTextGraphics();
+        textGraphics.setBackgroundColor(TextColor.Factory.fromString("#993399"));
+        textGraphics.putString(new TerminalPosition(wall.getPosition().getX(),wall.getPosition().getY())," ");
     }
 }
